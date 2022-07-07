7984aa65a7ff4145b765e1ff9033c14ffbf513a2
zorjfx <108087601+zorjfx@users.noreply.github.com>
2022-07-07 21:01:32 +0600

    Add files via upload

---
+++ homework#2122.py
@@ -0,0 +1,23 @@
+# 21
+
+# import random
+#
+# random_list1 = list(random.randint(0, 1000) for i in range(100))
+# random_list2 = list(random.randint(0, 1000) for j in range(100))
+#
+# print(set(random_list1) - set(random_list2))
+# print(set(random_list2) - set(random_list1))
+
+# 22
+
+# import random
+#
+# random_list1 = list(random.randint(0, 1000) for i in range(100))
+# random_list2 = list(random.randint(0, 1000) for j in range(100))
+#
+# def correlation(list1, list2):
+#     for i in random_list1:
+#         for j in random_list2:
+#             if len(set(str(i)) & set(str(j))) > 0:
+
+
