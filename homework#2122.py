71a1f25863492a44268d3163dfefffca366c0c46
zorjfx <r.a.zhilenko08@gmail.com>
2022-07-08 19:32:02 +0600

    фвфвфцвфйццфвфвф

--- homework#2122.py
+++ homework#2122.py
@@ -1,23 +1,31 @@
-# 21
-
 # import random
 #
 # random_list1 = list(random.randint(0, 1000) for i in range(100))
 # random_list2 = list(random.randint(0, 1000) for j in range(100))
 #
-# print(set(random_list1) - set(random_list2))
-# print(set(random_list2) - set(random_list1))
-
-# 22
-
-# import random
-#
-# random_list1 = list(random.randint(0, 1000) for i in range(100))
-# random_list2 = list(random.randint(0, 1000) for j in range(100))
 #
 # def correlation(list1, list2):
-#     for i in random_list1:
-#         for j in random_list2:
-#             if len(set(str(i)) & set(str(j))) > 0:
+#
+#     for i in range(min(len(list1), len(list2))):
+#         first, second = list1[i], list2[i]
+#
+#         if (first >= 0 and second >= 0) or (first <= 0 and second <= 0):
+#             correlations.append(1)
+#         else:
+#             correlations.append(-1)
+#
+#     if correlations.count(1) > correlations.count(-1):
+#         return 1
+#     elif correlations.count(1) < correlations.count(-1):
+#         return -1
+#     else:
+#         return 0
+#
+#
+# print(correlation(
+#     [1, 2, 3, 4],
+#     [-1, -22, -3, 4],
+# ))
+#
 
 
