695430ae230e12be859c4dcef7bb370d7669d52d
zorjfx <108087601+zorjfx@users.noreply.github.com>
2022-07-14 18:39:08 +0600

    homework#23
    с задачей про запрос я не знаю как использовать parse, так как пишет, что это объект, а как принимать именно json я не знаю. В калькуляторе не получается вывести результат

---
+++ homework#23.js
@@ -0,0 +1,55 @@
+//  a
+
+// let a = 10
+// let b = 4
+// let c = 7
+
+// console.log(a + b - c)
+
+// b
+
+// for (i = 0; i < 101; i++) {
+//     console.log('hello')
+// }
+
+// c
+
+// d
+
+// let result = 0
+
+// function sum (first_summand, second_summand){
+//     result = first_summand + second_summand
+//     document.getElementById("result").innerHTML = result
+//     return result
+// }
+
+// function dif (reduced, deductible){
+//     result =  reduced - deductible
+//     document.getElementById("result").innerHTML = result
+//     return result
+// }
+
+// function mul (first_multiplier, second_multiplier){
+//     result = first_multiplier * second_multiplier
+//     document.getElementById("result").innerHTML = result
+//     return result
+// }
+
+// function div (divisible, divider){
+//     result = divisible / divider
+//     document.getElementById("result").innerHTML = result
+//     return result
+// }
+
+// e
+
+// fetch('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT')
+//     .then((response) => {
+//         return response.json();
+//     })
+//     .then((data) => {
+//         result = JSON.parse(data)
+//         console.log(result("price"));
+
+//     });
