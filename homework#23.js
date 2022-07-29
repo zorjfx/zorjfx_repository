cde106dbca6d9ade75d5335cd117a51f7f38a623
zorjfx <r.a.zhilenko08@gmail.com>
2022-07-29 19:04:24 +0600

    homework#24

---
+++ homework#23.js
@@ -0,0 +1,55 @@
+//  a
+
+// let a = 10
+// let b = 4
+// let c = 7
+
+// console.log(a + b - c)
+
+// b
+
+// for (i = 0; i < 101; i++) {
+//     console.log('hello')
+// }
+
+// c
+
+// d
+
+// let result = 0
+
+// function sum (){
+//     result = document.getElementById("num1").innerHTML + document.getElementById("num2").innerHTML
+//     document.getElementById("result").innerHTML = result
+//     return result
+// }
+
+// function dif (reduced, deductible){
+//     result =  reduced - deductible
+//     document.getElementById("result").innerHTML = result
+//     return result
+// }
+
+// function mul (first_multiplier, second_multiplier){
+//     result = first_multiplier * second_multiplier
+//     document.getElementById("result").innerHTML = result
+//     return result
+// }
+
+// function div (divisible, divider){
+//     
+//  document.getElementById("result").innerHTML = divisible / divider
+//     
+// }
+
+// e
+
+// fetch('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT')
+//     .then((response) => {
+//         return response.json();
+//     })
+//     .then((data) => {
+//         result = JSON.parse(data)
+//         console.log(result("price"));
+
+//     });
